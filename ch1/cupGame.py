c0bf0ca07c38a826cb88a51bd6cf877f70420154
Nady Luna <nadyedja@gmail.com>
2021-11-04 21:03:46 -0300

    feature(studies)-creating dogCal.py

---
+++ ch1/cupGame.py
@@ -0,0 +1,19 @@
+Python 3.9.6 (v3.9.6:db3ff76da1, Jun 28 2021, 11:49:53) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+KeyboardInterrupt
+>>> 
+================================ RESTART: Shell ================================
+>>> cup1=0
+>>> cup2=1
+>>> cup3=0
+>>> cup1 =cup1 + 1
+>>> cup2=cup1-1
+>>> cup3=cup1
+>>> cup1=cup1*0
+>>> cup2=cup3
+>>> cup3=cup1
+>>> cup1 =cup2%1
+>>> cup3=cup2
+>>> cup2=cup3-cup3
--- ch1/dogcalc.py
+++ ch1/dogcalc.py
@@ -1,3 +1,4 @@
-dog_name = input ("whast is your dog's name? ")
-
-dog_age = input ("What is your dog's age? ")
+dog_name = input("Whats is your dog's name?")
+dog_age = input("whats is your dog's name?")
+human_age = int(dog_age) * 7
+print('Your dog', dog_name,'is', human_age, 'years old in human years')
